62082d855255edc38cd32cae44804e0d272b6ec6
CallMeDKay <s3812649@rmit.edu.vn>
2021-02-03 00:24:14 +0700

    Dynamic filtered content for comment box

--- client/src/components/ReviewCategoryBox.js
+++ client/src/components/ReviewCategoryBox.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './ReviewCategoryBox.css';
 import { Container } from '@material-ui/core';
+import CommentCard from './CommentCard';
 
-
-// Get the container element
 
 const ReviewCategoryBox = (props) => {
 
@@ -22,7 +21,8 @@ const ReviewCategoryBox = (props) => {
     /* === Define states === */
     const [cmtCategory, setCmtCategory] = useState(initialCategory); // list of cmt categoires
     const [selectedCategory, setSelectedCategory] = useState(''); // current selected category of comment
-    const [categoryContent, setCategoryContent] = useState(''); // placeholder for cmt content
+    const [commentContent, setCommentContent] = useState(''); // placeholder for cmt content
+    const [filteredCategoryContent, setFilteredCategoryContent] = useState(''); // placeholder for filtered content by category
 
     /* === Define methods and handlers === */
     const handleOnCategorySelect = (e) => {
@@ -47,11 +47,11 @@ const ReviewCategoryBox = (props) => {
         if(cmtCategory.length > 0) {
             setSelectedCategory(cmtCategory[0])
         }
-    }, [categoryContent]);
+    }, [commentContent]);
 
     /* Fetch list of comments by product's id (For ex: ID 1)*/
     useEffect(async () => {
-        setCategoryContent(
+        setCommentContent(
             {
                 'product_id': '1',
                 'comment': [
@@ -67,12 +67,12 @@ const ReviewCategoryBox = (props) => {
                         'user_id': '3812650',
                         'category': ['packaging'],
                         'rating': 5,
-                        'content': 'The packaging is quite nice and handled carefully'
+                        'content': 'The package is quite nice and handled carefully'
                     },
                     {
                         'cmt_id': '103',
                         'user_id': '3812651',
-                        'category': ['price, quality'],
+                        'category': ['price', 'quality'],
                         'rating': 3,
                         'content': 'The price is quite high but the cloth material is acceptable'
                     },
@@ -90,10 +90,31 @@ const ReviewCategoryBox = (props) => {
                         'rating': 5,
                         'content': 'Nice mixture of color btw, it would be even better if the shirt has bigger shoulder since im quite big'
                     },
+                    {
+                        'cmt_id': '106',
+                        'user_id': '3812654',
+                        'category': ['quality', 'package', 'price', 'shipment', 'other'],
+                        'rating': 5,
+                        'content': 'Everything is perfect !!'
+                    }
                 ]
             }
         )
     }, [])
+
+    /* Listen on category changes and filter content by category */
+    useEffect(() => {
+        if(commentContent) { // if exists contents
+            if(commentContent.comment) { // if exists comments
+                let filteredContent = commentContent.comment.filter((c) => { // filter content by categories
+                    let ctgs = [];
+                    c.category.map(ctg => ctgs.push(ctg));
+                    return ctgs.includes(selectedCategory.toLowerCase());
+                })
+                setFilteredCategoryContent(filteredContent);
+            }
+        }
+    }, [selectedCategory])
 
     return (
 			<React.Fragment>
@@ -101,6 +122,7 @@ const ReviewCategoryBox = (props) => {
 					<div className="review-box">
 						<nav id="single-nav" className="single-nav menu" role="navigation">
 							<ul>
+                                {/* Map comment categories */}
                                 {allowedCmtCategory.map((category, index) => {
                                     return (
                                             <li  key={index.toString()} >
@@ -112,7 +134,26 @@ const ReviewCategoryBox = (props) => {
 							</ul>
 						</nav>
                         <div className="review-box-content">
-                            This is description for box '{selectedCategory}'
+                            {/* Handle loading category content */}
+                            {filteredCategoryContent && selectedCategory ? 
+                            filteredCategoryContent.map((c, i) => {
+                                return (
+                                    <CommentCard key={i}>
+                                        <li><b>User ID:</b> {c.user_id}</li>
+                                        <li><b>Rating:</b> {c.rating}</li>
+                                        <li><b>Category:</b> {c.category.map(category => {
+                                            if(c.category.length > 1) {
+                                            return `${category} | `
+                                            }
+                                            return `${category}`
+                                            }
+                                            )}</li>
+                                        <li><b>Comment:</b> {c.content}</li>
+                                    </CommentCard>
+                                )
+                            })
+                            : 'undefined'
+                        }
                         </div>
 					</div>
 				</Container>
