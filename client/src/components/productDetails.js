b8bd4c65d3c6027e6b5b2fe7146672ebc649e72d
Truong Duy <s3818381@rmit.edu.vn>
2021-02-04 16:48:29 +0700

    API fetch data

--- client/src/components/productDetails.js
+++ client/src/components/productDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import './productDetails.css';
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import Link from "@material-ui/core/Link";
@@ -7,7 +7,27 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from '@material-ui/core/Container';
 
-export default function ProductDetails() {
+export default class ProductDetails extends React.Component {
+    
+  state = {
+    product: {
+      title: "Men Summer Retro Short Sleeve Loose Fit Shirt",
+      urlProduct:
+        "https://shopee.sg/Men-Summer-Retro-Short-Sleeve-Loose-Fit-Shirt-i.27121667.2166110314?",
+      urlPicture: "https://cf.shopee.sg/file/13534526cbb9ee6ed37f4696919b7756",
+      price: "$10.59",
+    },
+  };
+
+  async getProductDetails() {
+    //   Take JSON data from API and fetch into state
+    const url = "";
+    const response = await fetch(url);
+    this.setState({ product:  data.results});
+    const data = await response.json();
+  }
+
+  render() {
     return (
       <React.Fragment>
         {/* Head */}
@@ -20,7 +40,7 @@ export default function ProductDetails() {
               Homepage
             </Link>
             <Typography color="textPrimary">
-              Men Summer Retro Short Sleeve Loose Fit Shirt
+              {this.state.product.title}
             </Typography>
           </Breadcrumbs>
         </Container>
@@ -35,14 +55,15 @@ export default function ProductDetails() {
               <Grid item lg={5} sm={5}>
                 <img
                   id="img_product"
-                  src="https://cf.shopee.sg/file/13534526cbb9ee6ed37f4696919b7756"
+                  src={this.state.product.urlPicture}
+                  alt="product pic"
                 ></img>
               </Grid>
               {/* End the image */}
               {/* Start the infor */}
               <Grid item lg={7} sm={7}>
-                <p id="title">Men Summer Retro Short Sleeve Loose Fit Shirt</p>
-                <p id="price">$10.59</p>
+                <p id="title">{this.state.product.title}</p>
+                <p id="price">{this.state.product.price}</p>
                 <div style={{ paddingTop: "50px" }}>
                   <a
                     id="normal_text"
@@ -51,14 +72,12 @@ export default function ProductDetails() {
                     <img
                       id="guarantee"
                       src="https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/bca2e6323e918c445dfe0279ebbc2c39.png"
+                      alt="Shopee Guarantee"
                     />
                     <span id="text_guarantee">Shopee Guarantee</span>
                   </a>
                 </div>
-                <form
-                  action="https://shopee.sg/Men-Summer-Retro-Short-Sleeve-Loose-Fit-Shirt-i.27121667.2166110314"
-                  method="get"
-                >
+                <form action={this.state.product.urlProduct} method="get">
                   <button id="btn_buy" type="submit">
                     <span style={{ color: "white" }}>Buy Now</span>
                   </button>
@@ -71,4 +90,5 @@ export default function ProductDetails() {
         {/* End the Product Infor */}
       </React.Fragment>
     );
+  }
 }
