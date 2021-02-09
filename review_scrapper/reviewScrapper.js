ece1ba6e8907f9499ecdad3190606c4b27819c9a
hungnguyen <hungthezorba@gmail.com>
2021-02-09 16:34:04 +0700

    Scrape all reviews's content

--- review_scrapper/reviewScrapper.js
+++ review_scrapper/reviewScrapper.js
@@ -13,12 +13,12 @@ function wait (ms) {
 }
 
 const url =
-  "https://shopee.sg/Baby-Educational-Soft-Rubber-Hand-Grasp-Rolling-Ball-Crib-Mobile-Bell-Toy-i.87219179.2128867533";
+  "https://shopee.sg/Rigorer-Pro-Performance-Short-Sleeve-Compression-T-Shirt-SC0602-(Sports-Top-Gym-Fitness-Exercise-Cycling-Running)-i.106407924.4046257393";
 
 
 
   (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
 
   const navigationPromise = page.waitForNavigation();
@@ -37,26 +37,47 @@ const url =
      await page.evaluate(_viewportHeight => {
        window.scrollBy(0, _viewportHeight);
      }, viewportHeight);
-     await wait(2000);
+     await wait(1000);
      viewportIncr = viewportIncr + viewportHeight;
    }
  
    // Scroll back to to
    // Some extra delay to let images load
 
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(1000);
     await navigationPromise;
 
-    const reviews = await page.evaluate(() => {
+    const reviews = await page.evaluate( async () => {
+
+      function wait (ms) {
+        return new Promise(resolve => setTimeout(() => resolve(), ms));
+      }
 
       let results = []
 
-      let items = document.getElementsByClassName("shopee-product-rating__content")
-      for (let elem of items) {
-        let review = {} // Review object
-        review['content'] = elem.innerText // Add review content
-        results.push(review); // Add to the result list
+      let pages = document.getElementsByClassName("shopee-icon-button--right")
+
+      let trackingPage = 1;
+      let nextPage = 1;
+      while (trackingPage == nextPage && pages[0] != undefined){
+        let items = document.getElementsByClassName("shopee-product-rating__content")
+        for (let elem of items) {
+          let review = {} // Review object
+          review['content'] = elem.innerText // Add review content
+          results.push(review); // Add to the result list
+        }
+
+        await pages[0].click()
+        trackingPage = parseInt(document.getElementsByClassName("shopee-button-solid shopee-button-solid--primary")[0].innerText)
+        nextPage++;
+        await wait(500)
+        pages = null
+        pages = document.getElementsByClassName("shopee-icon-button--right")
       }
+
+     
+      
+      
       return (results)
     })
     console.log(reviews)
