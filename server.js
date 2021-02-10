bfe8e1c836ca3b2833b896062e38b8f16cb2f1cf
hungnguyen <hungthezorba@gmail.com>
2021-02-10 15:48:22 +0700

    Add API

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const os = require('os');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const review = require('./review_scrapper/index.js');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  
+  }
+
+/* ---- Define main endpoints ---- */
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to ReviewOrNot's API"
+    })
+})
+
+app.use("/api", review);
+ 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`)
+});
