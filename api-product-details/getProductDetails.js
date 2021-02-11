980730afba3aeaebe81a06bebc8967e521a36129
Truong Duy <s3818381@rmit.edu.vn>
2021-02-11 23:48:11 +0700

    complete scrapping data from product page

--- api-product-details/getProductDetails.js
+++ api-product-details/getProductDetails.js
@@ -6,29 +6,21 @@ const getProductDetails = async (searchQuery) => {
   const page = await browser.newPage();
   await page.goto(searchQuery);
 
-  // Pause the function for 6s
+  // Pause the function for 7s
   function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  }
-  await sleep(6500);
+    return new Promise((resolve) => { setTimeout(resolve, ms); });}
+  await sleep(7000);
 
   // Click the "I AM OVER 18" if the dialog shows in the page
-  if ((await page.$(".shopee-alert-popup__message")) !== null)
-    await page.click(".shopee-alert-popup__btn");
+  if ((await page.$(".shopee-alert-popup__message")) !== null) await page.click(".shopee-alert-popup__btn");
 
   // Get all Specifications
-  const specifications = await page.$$eval("._2gVYdB > label", (cates) =>
-    cates.map((cate) => cate.textContent)
-  );
+  const specifications = await page.$$eval("._2gVYdB > label", (cates) => cates.map((cate) => cate.textContent));
   specifications.shift();
   specifications.shift();
 
   // Get all Details
-  const details = await page.$$eval("._2gVYdB > div", (cates) =>
-    cates.map((cate) => cate.textContent)
-  );
+  const details = await page.$$eval("._2gVYdB > div", (details) => details.map((detail) => detail.textContent));
   details.shift();
 
   // Array to hold all our results
@@ -36,46 +28,30 @@ const getProductDetails = async (searchQuery) => {
 
   // Take each element and store it
   // Title
-  dataObj["title"] = await page.$eval(
-    "._3ZV7fL > span",
-    (text) => text.textContent
-  );
+  dataObj["title"] = await page.$eval("._3ZV7fL > span", (text) => text.textContent);
 
   // URL image
-  dataObj["imageUrl"] = await page.$eval("._39-Tsj > div", (div) =>
-    div
-      .getAttribute("style")
-      .substring(23, div.getAttribute("style").indexOf(")") - 1)
-  );
+  dataObj["imageUrl"] = await page.$eval("._39-Tsj > div", (div) => div.getAttribute("style").substring(23, div.getAttribute("style").indexOf(")") - 1));
 
   // Rating - 2 conditions to check if there are ratings or not
-  if ((await page.$("._3WXigY")) !== null) {
-    dataObj["rating"] = await page.$eval(
-      "._3WXigY",
-      (text) => text.textContent
-    );
-  } else dataObj["rating"] = null;
+  if ((await page.$("._3WXigY")) !== null) dataObj["rating"] = await page.$eval("._3WXigY", (text) => parseFloat(text.textContent));
+  else dataObj["rating"] = null;
 
   // Price
   dataObj["price"] = await page.$eval(".AJyN7v", (text) => text.textContent);
 
   // Category
-  dataObj["category"] = await page.$$eval("._2gVYdB > ._1qYtEg > a", (cates) =>
-    cates.map((cate) => cate.textContent)
-  );
+  dataObj["category"] = await page.$$eval("._2gVYdB > ._1qYtEg > a", (cates) => cates.map((cate) => cate.textContent));
   dataObj["category"].shift();
 
   // Brand
-  dataObj["brand"] = await page.$eval(
-    "._2gVYdB > a",
-    (text) => text.textContent
-  );
+  dataObj["brand"] = await page.$eval("._2gVYdB > a", (text) => text.textContent);
   if (dataObj["brand"] == "No Brand") dataObj["brand"] = null;
 
   // Stock
   for (var i = 0; i < specifications.length; i++) {
     if(specifications[i] == "Stock") {
-      dataObj["stock"] = details[details.length - specifications.length + i];
+      dataObj["stock"] = parseInt(details[details.length - specifications.length + i]);
       details.splice(details.length - specifications.length + i, 1);
       specifications.splice(i, 1);
       break;
@@ -83,17 +59,24 @@ const getProductDetails = async (searchQuery) => {
   }
 
   // Other specifications
-  dataObj["specification"] = {};
-  for (var i = details.length - 1; i >= 0; i--) {
-    index = details.length - 1 - i;
-    dataObj["specification"][specifications[specifications.length - 1 - index]] = details[i];
+  if (details.length == 0) dataObj["specification"] = null;
+  else {
+    dataObj["specification"] = {};
+    for (var i = details.length - 1; i >= 0; i--) {
+      index = details.length - 1 - i;
+      dataObj["specification"][specifications[specifications.length - details.length + i].toLowerCase()] = details[i];
+    }
+    if (specifications.length != details.length) {
+      const model = await page.$$eval("._3yEY86", (models) => models.map((model) => model.innerHTML));
+      dataObj["specification"]["model"] = model[1];
+    }
   }
 
-  // Description
-  dataObj["description"] = await page.$eval(
-    "._36_A1j > span",
-    (text) => text.textContent
-  );
+    // Description
+    dataObj["description"] = await page.$eval(
+      "._36_A1j > span",
+      (text) => text.textContent
+    );
 
   await browser.close();
   return dataObj;
